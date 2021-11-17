f079bbabc4e5d6e359e976fa2ad71e9d9c060eec
CarterT27 <carter.tran@gmail.com>
2021-11-16 23:02:24 -0700

    Added render function and function comments

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
 
+# Creates an array with the given dimensions that is filled with zeros
 def dead_state(board_width, board_height):
     board_state = [[0 for x in range(board_width)] for y in range(board_height)]
     return board_state
 
+# Fills the zeros of a dead_state board with 0s and 1s
 def random_state(board_width, board_height):
     board_state = dead_state(board_width, board_height)
     for y in range(board_height):
@@ -16,5 +18,18 @@ def random_state(board_width, board_height):
             board_state[x][y] = cell_state
     return board_state
 
-board = random_state(5,5)
-print(board)
+# Formats the board state and prints it to the terminal
+def render(board_state):
+    for y in range(len(board_state) + 1):
+        print("--", end = "")
+    print()
+
+    for y in range(len(board_state)):
+        print("|", end = "")
+        for x in range(len(board_state[y])):
+            print(str(board_state[x][y]) + " ", end = "")
+        print("|")
+
+    for y in range(len(board_state) + 1):
+        print("--", end = "")
+    print()
